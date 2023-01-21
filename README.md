c4bc574005975092745f4e7f8bb64dccbcc604a1
Frank Rosner <frank.rosner@datastax.com>
2023-01-21 20:42:33 +0100

    more improvements
    - send message optional
    - archive optional
    - skip archived channels
    - ignore self joining

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ Slack app to automatically archive Slack channels.
 ## Usage
 
 ```bash
-SLACK_BEARER_TOKEN="xoxb-not-a-real-token" \
-  SLACK_SIGNING_SECRET="abcdefgnotarealsecret" \
-  SLACK_CHANNEL_REGEX='^tmp-.*' \
-  SLACK_LAST_MESSAGE_MAX_AGE='30d' \
+HM_SLACK_BEARER_TOKEN="xoxb-not-a-real-token" \
+  HM_SLACK_SIGNING_SECRET="abcdefgnotarealsecret" \
+  HM_CHANNEL_REGEX='^tmp-.*' \
+  HM_LAST_MESSAGE_MAX_AGE='30d' \
+  HM_ARCHIVE_CHANNELS='true' \
+  HM_SEND_MESSAGE='true' \
   node index.js
 ```
 
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const { App } = require('@slack/bolt');
 const parseDuration = require('parse-duration')
 
 const app = new App({
-    token: process.env.SLACK_BEARER_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET
+    token: process.env.HM_SLACK_BEARER_TOKEN,
+    signingSecret: process.env.HM_SLACK_SIGNING_SECRET
 });
 
-const channelRegex = process.env.SLACK_CHANNEL_REGEX;
-const lastMessageMaxAge = parseDuration(process.env.SLACK_LAST_MESSAGE_MAX_AGE);
+const sendMessage = process.env.HM_SEND_MESSAGE;
+const archiveChannels = process.env.HM_ARCHIVE_CHANNELS;
+const channelRegex = process.env.HM_CHANNEL_REGEX;
+const lastMessageMaxAge = parseDuration(process.env.HM_LAST_MESSAGE_MAX_AGE);
 console.log(`Archiving channels matching ${channelRegex} with no activity for ${lastMessageMaxAge}ms`);
 
 const now = Date.now();
@@ -18,20 +20,36 @@ const now = Date.now();
     const matchingChannels = channels.filter(c => c.name.match(channelRegex) != null);
     await Promise.all(matchingChannels.map(async (c) => {
         const channelName = `${c.name} (${c.id})`;
-        console.log(`Joining channel ${channelName}`);
-        await app.client.conversations.join({channel: c.id});
-        console.log(`Getting latest message from channel ${channelName}`);
-        const messages = await app.client.conversations.history({channel: c.id, limit: 1});
-        const lastMessageTs = messages.messages[0].ts * 1000; // we want ms precision
-        const lastMessageAge = now - lastMessageTs;
-        if (lastMessageAge > lastMessageMaxAge) {
-            const archiveMsg = `Archiving channel ${channelName} as the last message is ${lastMessageAge}ms old (max age = ${lastMessageMaxAge}ms)`;
-            console.log(archiveMsg);
-            await app.client.chat.postMessage({
-                channel: c.id,
-                text: archiveMsg
-            })
-            await app.client.conversations.archive({channel: c.id});
+        if (c.is_channel && !c.is_archived) {
+            if (!c.is_member) {
+                console.log(`Joining channel ${channelName}`);
+                await app.client.conversations.join({channel: c.id});
+            }
+            console.log(`Getting latest message from channel ${channelName}`);
+            const messages = await app.client.conversations.history({channel: c.id, limit: 2});
+            let lastMessage = messages.messages[0];
+            if (lastMessage.subtype === 'channel_join' && messages.messages.length > 1) {
+                // If the most recent message is someone joining, it might be us, so we look at the last but one message
+                lastMessage = messages.messages[1]
+            }
+            const lastMessageTs = lastMessage.ts * 1000; // we want ms precision
+            const lastMessageAge = now - lastMessageTs;
+            if (lastMessageAge > lastMessageMaxAge) {
+                console.log(`In channel ${channelName}, the last message is ${lastMessageAge}ms old (max age = ${lastMessageMaxAge}ms)`);
+                if (sendMessage === 'true') {
+                    console.log(`Sending message to channel ${channelName}`);
+                    await app.client.chat.postMessage({
+                        channel: c.id,
+                        text: `I am archiving #${c.name} because it has been inactive for a while. Please unarchive the channel and reach out to my owner if this was a mistake!`
+                    })
+                }
+                if (archiveChannels === 'true') {
+                    console.log(`Archiving channel ${channelName}`);
+                    await app.client.conversations.archive({channel: c.id});
+                }
+            } else {
+                console.log(`Not doing anything with ${channelName}, as there is still recent activity`)
+            }
         }
     }));
 })();
